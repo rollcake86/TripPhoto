fcc51954ec1905330a3a9aeb105c252a91766ace
rollcake86 <ikinye86@gmail.com>
2023-03-06 22:51:51 +0900

    - detail page

--- app/src/main/java/com/rollcake/tripPhoto/network/NetworkUtils.kt
+++ app/src/main/java/com/rollcake/tripPhoto/network/NetworkUtils.kt
@@ -20,3 +20,20 @@ fun parseJsonResult(jsonResult: JSONObject): ArrayList<TripProperty> {
 
     return tripList
 }
+
+
+
+fun contentTypeId(id : String) : String {
+//    관광타입(75:레포츠, 76:관광지, 77:교통, 78:문화시설, 79:쇼핑, 80:숙박, 82:음식점, 85:축제공연행사) ID
+    when(id) {
+        "75" -> return "Sport"
+        "76" -> return "Place"
+        "77" -> return "Traffic"
+        "78" -> return "Hot Place"
+        "79" -> return "Shopping"
+        "80" -> return "Hotel"
+        "82" -> return "Food"
+        "85" -> return "Festival"
+    }
+    return "Sport"
+}
--- app/src/main/java/com/rollcake/tripPhoto/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/rollcake/tripPhoto/ui/detail/DetailFragment.kt
@@ -1,20 +1,27 @@
 package com.rollcake.tripPhoto.ui.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.rollcake.tripPhoto.R
 import com.rollcake.tripPhoto.base.BaseFragment
 import com.rollcake.tripPhoto.base.NavigationCommand
 import com.rollcake.tripPhoto.databinding.FragmentDetailBinding
 import com.rollcake.tripPhoto.databinding.FragmentSettingBinding
+import com.rollcake.tripPhoto.network.contentTypeId
 import com.rollcake.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
+import timber.log.Timber
 
 class DetailFragment : BaseFragment() {
 
@@ -25,15 +32,32 @@ class DetailFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_detail, container, false)
-
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
 
         val trip = DetailFragmentArgs.fromBundle(requireArguments()).selectedTrip
-
-
+        Glide.with(this.requireContext()).load(trip.firstimage).into(view.findViewById<ImageView>(R.id.imageView2))
         binding.trip = trip
         binding.lifecycleOwner = this
+
+        view.findViewById<ImageView>(R.id.help_button).setOnClickListener {
+            val gmmIntentUri =
+                Uri.parse("geo:${trip.mapy},${trip.mapx}?q=${trip.title}")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+
+        view.findViewById<TextView>(R.id.trip_title_textview).text = trip.title
+        view.findViewById<TextView>(R.id.trip_addr_textview).text = trip.addr1
+        view.findViewById<TextView>(R.id.trip_dist_textview).text = "${trip.dist.toDouble().toInt()} m"
+        view.findViewById<TextView>(R.id.trip_tel_textview).text = trip.tel
+        view.findViewById<TextView>(R.id.trip_type_textview).text = contentTypeId(trip.contenttypeid)
+
+        view.findViewById<ImageView>(R.id.save_btn).setOnClickListener {
+
+
+        }
 
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
